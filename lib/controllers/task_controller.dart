8ce90a4bdb428e8330dbe248646c58420f8082e1
Patrick <patricknaashat@yahoo.com>
2023-08-14 14:43:33 +0300

    first commit

---
+++ lib/controllers/task_controller.dart
@@ -0,0 +1,37 @@
+import 'package:get/get.dart';
+import 'package:to_do_app/db/db_helper.dart';
+
+import '../models/task.dart';
+
+class TaskController extends GetxController {
+  final RxList<Task> taskList = <Task>[].obs;
+
+  // get data from database
+  Future<void> getTasks() async {
+    final List<Map<String, dynamic>> tasks = await DBHelper.query();
+
+    taskList.assignAll(tasks.map((task) => Task.fromJson(task)).toList());
+  }
+
+  // delete data from database
+  void deleteTask(Task task) async {
+    await DBHelper.delete(task);
+    getTasks();
+  }
+
+  void deleteAllTasks() async {
+    await DBHelper.deleteAll();
+    getTasks();
+  }
+
+  // update data from database
+  void markTaskCompleted(int id) async {
+    await DBHelper.update(id);
+    getTasks();
+  }
+
+  // add task to database
+  Future<int> addTask({required Task task}) {
+    return DBHelper.insert(task);
+  }
+}
---
+++ lib/ui/pages/notification_screen.dart
@@ -0,0 +1,158 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '/ui/theme.dart';
+
+class NotificationScreen extends StatefulWidget {
+  const NotificationScreen({Key? key, required this.payload}) : super(key: key);
+
+  final String payload;
+
+  @override
+  _NotificationScreenState createState() => _NotificationScreenState();
+}
+
+class _NotificationScreenState extends State<NotificationScreen> {
+  String _payload = '';
+
+  @override
+  void initState() {
+    super.initState();
+    _payload = widget.payload;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // ignore: deprecated_member_use
+      backgroundColor: context.theme.backgroundColor,
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () => Get.back(),
+          icon: Icon(
+            Icons.arrow_back_ios,
+            color: Get.isDarkMode ? Colors.white : darkGreyClr,
+          ),
+        ),
+        elevation: 0,
+        // ignore: deprecated_member_use
+        backgroundColor: context.theme.backgroundColor,
+        title: Text(
+          _payload.toString().split('|')[0],
+          style: TextStyle(color: Get.isDarkMode ? Colors.white : darkGreyClr),
+        ),
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            const SizedBox(height: 20),
+            Column(
+              children: [
+                Text(
+                  'Hello, Patrick',
+                  style: TextStyle(
+                    fontSize: 26,
+                    fontWeight: FontWeight.w900,
+                    color: Get.isDarkMode ? Colors.white : darkGreyClr,
+                  ),
+                ),
+                const SizedBox(height: 10),
+                Text(
+                  'You have a new reminder',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w300,
+                    color: Get.isDarkMode ? Colors.grey[100] : darkGreyClr,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 10),
+            Expanded(
+              child: Container(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 30, vertical: 30),
+                margin: const EdgeInsets.symmetric(horizontal: 30),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  color: primaryClr,
+                ),
+                child: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Row(
+                        children: [
+                          Icon(
+                            Icons.text_format,
+                            size: 30,
+                            color: Colors.white,
+                          ),
+                          SizedBox(width: 20),
+                          Text(
+                            'Title',
+                            style: TextStyle(fontSize: 30, color: Colors.white),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 20),
+                      Text(
+                        _payload.toString().split('|')[0],
+                        style:
+                            const TextStyle(color: Colors.white, fontSize: 20),
+                      ),
+                      const SizedBox(height: 20),
+                      const Row(
+                        children: [
+                          Icon(
+                            Icons.description,
+                            size: 30,
+                            color: Colors.white,
+                          ),
+                          SizedBox(width: 20),
+                          Text(
+                            'Description',
+                            style: TextStyle(fontSize: 30, color: Colors.white),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 20),
+                      Text(
+                        _payload.toString().split('|')[1],
+                        style:
+                            const TextStyle(color: Colors.white, fontSize: 20),
+                        textAlign: TextAlign.justify,
+                      ),
+                      const SizedBox(height: 20),
+                      const Row(
+                        children: [
+                          Icon(
+                            Icons.calendar_today_outlined,
+                            size: 30,
+                            color: Colors.white,
+                          ),
+                          SizedBox(width: 20),
+                          Text(
+                            'Date',
+                            style: TextStyle(fontSize: 30, color: Colors.white),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 20),
+                      Text(
+                        _payload.toString().split('|')[2],
+                        style:
+                            const TextStyle(color: Colors.white, fontSize: 20),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 10),
+          ],
+        ),
+      ),
+    );
+  }
+}
